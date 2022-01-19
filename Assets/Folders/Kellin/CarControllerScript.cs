e947939c19930215049a73854397deb00af69578
KellinSchaap <200557@student.glu.nl>
2022-01-19 12:43:46 +0100

    Work in progress drift functie voor de auto

--- Assets/Folders/Kellin/CarControllerScript.cs
+++ Assets/Folders/Kellin/CarControllerScript.cs
@@ -19,6 +19,9 @@ public class CarControllerScript : MonoBehaviour
     public float m_groundDrag = 3f;     // Drag when the car is on the ground
     public float m_airDrag = 0.3f;      // Drag when the car is in the air
 
+    private bool m_isDrifting = false;  // Is the car using a drift
+    private float m_driftDirection;     // In which direction does the car drift to
+
     [Header("Ground Check")]
     public float m_gravityForce = 10f;      // The gravity force to the car
     public float m_checkGroundRayLength = 0.5f;     // Length of the ground Ray
@@ -43,9 +46,31 @@ public class CarControllerScript : MonoBehaviour
             m_speedInput = Input.GetAxis("Vertical") * m_reverseAccel * 1000f;
         }
 
+        if(Input.GetKeyDown(KeyCode.Space) && !m_isDrifting && Input.GetAxis("Horizontal") != 0)
+        {
+            m_isDrifting = true;
+            m_driftDirection = Input.GetAxis("Horizontal") > 0 ? 1 : -1;     // If Input.Axis > 0, return 1 else return -1
+        }
+
+        if (m_isDrifting)
+        {
+            float control;
+            
+            if(m_driftDirection == 1)
+            {
+                control = ExtensionMethods.Remap(Input.GetAxis("Horizontal"), -1, 1, 0, 2);
+            }
+            else
+            {
+                control = ExtensionMethods.Remap(Input.GetAxis("Horizontal"), -1, 1, 2, 0);
+            }
+
+        }
+
+
         m_rotationInput = Input.GetAxis("Horizontal");
 
-        if (m_isGrounded)
+        if (IsCarGrounded())
         {
             transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles + new Vector3(0f, m_rotationInput * m_rotateSpeed * Time.deltaTime * Input.GetAxis("Vertical"), 0f));
         }
@@ -55,18 +80,7 @@ public class CarControllerScript : MonoBehaviour
 
     private void FixedUpdate()
     {
-        m_isGrounded = false;
-
-        RaycastHit hit;
-
-        if (Physics.Raycast(m_groundRayPos.position, -transform.up, out hit, m_checkGroundRayLength, m_groundLayer))
-        {
-            m_isGrounded = true;
-
-            transform.rotation = Quaternion.FromToRotation(transform.up, hit.normal) * transform.rotation;
-        }
-
-        if (m_isGrounded)
+        if (IsCarGrounded())
         {
             m_sphereBody.drag = m_groundDrag;
 
@@ -81,5 +95,22 @@ public class CarControllerScript : MonoBehaviour
 
             m_sphereBody.AddForce(Vector3.up * -m_gravityForce * 100);
         }
+    }
+
+    // Is Car Grounded will check if the groundRay is hitting a ground layer, if true return a true for m_isGrounded 
+    private bool IsCarGrounded()
+    {
+        m_isGrounded = false;
+
+        RaycastHit hit;
+
+        if (Physics.Raycast(m_groundRayPos.position, -transform.up, out hit, m_checkGroundRayLength, m_groundLayer))
+        {
+            m_isGrounded = true;
+
+            transform.rotation = Quaternion.FromToRotation(transform.up, hit.normal) * transform.rotation;
+        }
+
+        return m_isGrounded;
     }
 }
