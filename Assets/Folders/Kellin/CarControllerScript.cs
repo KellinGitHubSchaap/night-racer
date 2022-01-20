caaa17c94ee2aeedf6c70c645ef146e28cb38068
KellinSchaap <200557@student.glu.nl>
2022-01-20 11:21:31 +0100

    Auto kan driften

--- Assets/Folders/Kellin/CarControllerScript.cs
+++ Assets/Folders/Kellin/CarControllerScript.cs
@@ -11,16 +11,16 @@ public class CarControllerScript : MonoBehaviour
     public float m_forwardAccel = 8f;       // Forward acceleration speed
     public float m_reverseAccel = 4f;       // Backward acceleration speed
 
-    public float m_rotateSpeed = 180f;      // Rotation speed of the car
+    [Tooltip("Rotation Speed of the car")]
+    private float m_mobility = 60f;      // Rotation speed of the car
+    public float m_minMobility = 60f;      // Rotation speed of the car
+    public float m_maxMobility = 180f;      // Rotation speed of the car
 
     private float m_speedInput;        
     private float m_rotationInput;
 
     public float m_groundDrag = 3f;     // Drag when the car is on the ground
     public float m_airDrag = 0.3f;      // Drag when the car is in the air
-
-    private bool m_isDrifting = false;  // Is the car using a drift
-    private float m_driftDirection;     // In which direction does the car drift to
 
     [Header("Ground Check")]
     public float m_gravityForce = 10f;      // The gravity force to the car
@@ -46,33 +46,20 @@ public class CarControllerScript : MonoBehaviour
             m_speedInput = Input.GetAxis("Vertical") * m_reverseAccel * 1000f;
         }
 
-        if(Input.GetKeyDown(KeyCode.Space) && !m_isDrifting && Input.GetAxis("Horizontal") != 0)
-        {
-            m_isDrifting = true;
-            m_driftDirection = Input.GetAxis("Horizontal") > 0 ? 1 : -1;     // If Input.Axis > 0, return 1 else return -1
-        }
-
-        if (m_isDrifting)
-        {
-            float control;
-            
-            if(m_driftDirection == 1)
-            {
-                control = ExtensionMethods.Remap(Input.GetAxis("Horizontal"), -1, 1, 0, 2);
-            }
-            else
-            {
-                control = ExtensionMethods.Remap(Input.GetAxis("Horizontal"), -1, 1, 2, 0);
-            }
-
-        }
-
-
         m_rotationInput = Input.GetAxis("Horizontal");
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            m_mobility = 130;
+        }
+        else if(Input.GetKeyUp(KeyCode.Space))
+        {
+            m_mobility = 60;
+        }
 
         if (IsCarGrounded())
         {
-            transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles + new Vector3(0f, m_rotationInput * m_rotateSpeed * Time.deltaTime * Input.GetAxis("Vertical"), 0f));
+            transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles + new Vector3(0f, m_rotationInput * m_mobility * Time.deltaTime * Input.GetAxis("Vertical"), 0f));
         }
 
         transform.position = m_sphereBody.transform.position;
@@ -95,6 +82,11 @@ public class CarControllerScript : MonoBehaviour
 
             m_sphereBody.AddForce(Vector3.up * -m_gravityForce * 100);
         }
+    }
+
+    public void Steering(int direction, float amount) 
+    {
+
     }
 
     // Is Car Grounded will check if the groundRay is hitting a ground layer, if true return a true for m_isGrounded 
