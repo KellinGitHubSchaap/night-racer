174e82db39cc9a2fc3ed3d1cc9c5e4c5cd2f4e52
AlexV03 <alexverstraten03@gmail.com>
2022-01-21 12:44:58 +0100

    destructible enviroment done

--- Assets/Folders/Alex/Scripts/Destructible.cs
+++ Assets/Folders/Alex/Scripts/Destructible.cs
@@ -10,6 +10,10 @@ public class Destructible : MonoBehaviour
     [SerializeField] private GameObject destroyedVersion;
     [Tooltip("How fast a object must go to break it")]
     [SerializeField] private float breakSpeed = 20;
+    [Tooltip("The breakable layer")]
+    [SerializeField] private LayerMask destructibleLayer;
+
+    private bool broken;
 
     private void OnCollisionEnter(Collision collision)
     {
@@ -17,13 +21,36 @@ public class Destructible : MonoBehaviour
         if (collision.gameObject.GetComponent<Rigidbody>().velocity.magnitude * 3.6f > breakSpeed)
         {
             Debug.Log("AAAAAAAAAAAAAAA");
-            Break();
+            //Break();
         }
     }
 
-    public void Break()
+    private void OnTriggerEnter(Collider other)
+    {
+        if (broken)
+            return;
+
+        if (other.gameObject.GetComponent<Rigidbody>().velocity.magnitude * 3.6f > breakSpeed)
+        {
+            broken = true;
+            Break(other.bounds.ClosestPoint(other.gameObject.transform.position));
+        }
+    }
+
+    /// <summary>
+    /// This is called to spawn the broken object and delete the current object
+    /// </summary>
+    public void Break(Vector3 hitPosition)
     {
         Instantiate(destroyedVersion, transform.position, transform.rotation);
+        Collider[] colliderPieces = Physics.OverlapSphere(hitPosition, 4, destructibleLayer);
+
+        foreach (Collider item in colliderPieces)
+        {
+            Rigidbody rb = item.gameObject.GetComponent<Rigidbody>();
+            Debug.Log(rb.gameObject);
+            rb.AddExplosionForce(250, hitPosition, 5);
+        }
         Destroy(gameObject);
     }
 }
