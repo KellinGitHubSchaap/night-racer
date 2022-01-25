b781f06ea62c51fe6dd58c1bba77f1220e8251ef
KellinSchaap <200557@student.glu.nl>
2022-01-25 08:56:26 +0100

    Auto carrosserie beweegt bij het sturen

--- Assets/Folders/Kellin/Scripts/CarControllerScript.cs
+++ Assets/Folders/Kellin/Scripts/CarControllerScript.cs
@@ -41,12 +41,18 @@ public class CarControllerScript : MonoBehaviour
     public LayerMask m_groundLayer;                 // What layer is considered ground
     private bool m_isGrounded;                      // Is the car grounded
 
-    [Header("Car Parts")]
+    [Header("Car Parts : Wheels")]
     public Transform m_frontWheelRight;
     public Transform m_frontWheelLeft;
 
     public float m_minWheelRotation = 15f;      // If the car isn't drifting a less sharp tire angle is needed
     public float m_maxWheelRotation = 25f;      // If the car is drifting a sharper tire angle is needed
+
+    [Header("Car Parts : Body")]
+    public Transform m_carBody;
+
+    public float m_minBanking = 5f;        // How much does the car bank to the side when not drifting
+    public float m_maxBanking = 10f;        // How much does the car bank to the side when drifting
 
     [Header("Other Settings")]
     private float m_offsetToCenterSphere = -.2f;
@@ -107,7 +113,7 @@ public class CarControllerScript : MonoBehaviour
         m_accel = m_speedInput < 0 ? m_accelBoost : m_maxForwardAccel;          // Change accel based on the current SpeedInput of the car 
 
         m_speedInput = Mathf.Clamp(m_speedInput, (-reverseSpeed * 67.629750f) / 1.03f, (maxSpeed * 67.629750f) / 1.03f);                  // Clamp the speed  //1kmp 67,629750
-        
+
         if (!m_isDrifting)
         {
             m_mobility = Mathf.Clamp(m_mobility, m_minMobility, m_maxMobility);     // Clamp the mobility
@@ -143,13 +149,17 @@ public class CarControllerScript : MonoBehaviour
             // If the car is drifting turn the frontwheels
             if (!m_isDrifting)
             {
-                m_frontWheelLeft.localEulerAngles = new Vector3(0, (Input.GetAxis("Horizontal") * m_minWheelRotation), m_frontWheelLeft.localEulerAngles.z);
-                m_frontWheelRight.localEulerAngles = new Vector3(0, (Input.GetAxis("Horizontal") * m_minWheelRotation), m_frontWheelRight.localEulerAngles.z);
+                m_frontWheelLeft.localEulerAngles = new Vector3(0, (Input.GetAxis("Horizontal") * m_minWheelRotation + 180), 0);
+                m_frontWheelRight.localEulerAngles = new Vector3(0, (Input.GetAxis("Horizontal") * m_minWheelRotation) + 180, 0);
+
+                m_carBody.localEulerAngles = new Vector3(0, m_carBody.localEulerAngles.y, Input.GetAxis("Horizontal") * m_minBanking);
             }
             else
             {
-                m_frontWheelLeft.localEulerAngles = new Vector3(0, (Input.GetAxis("Horizontal") * m_maxWheelRotation), m_frontWheelLeft.localEulerAngles.z);
-                m_frontWheelRight.localEulerAngles = new Vector3(0, (Input.GetAxis("Horizontal") * m_maxWheelRotation), m_frontWheelRight.localEulerAngles.z);
+                m_frontWheelLeft.localEulerAngles = new Vector3(0, (Input.GetAxis("Horizontal") * m_maxWheelRotation) + 180, 0);
+                m_frontWheelRight.localEulerAngles = new Vector3(0, (Input.GetAxis("Horizontal") * m_maxWheelRotation) + 180, 0);
+
+                m_carBody.localEulerAngles = new Vector3(0, m_carBody.localEulerAngles.y, Input.GetAxis("Horizontal") * m_maxBanking);
             }
         }
 
