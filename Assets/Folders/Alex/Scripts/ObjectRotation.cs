9255edf971320016e027fd031ad6f11035652357
AlexV03 <78423170+AlexV03@users.noreply.github.com>
2022-01-26 09:47:05 +0100

    rotation

--- Assets/Folders/Alex/Scripts/ObjectRotation.cs
+++ Assets/Folders/Alex/Scripts/ObjectRotation.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 public class ObjectRotation : MonoBehaviour
 {
     [SerializeField] private float rotationSpeed = 5;
+    [SerializeField] private Transform rotatedObject;
+    [SerializeField] private Vector3 rotationDirection;
 
     private void Update()
     {
-        
+        rotatedObject.Rotate(rotationDirection, rotationSpeed * Time.deltaTime);
     }
 }
